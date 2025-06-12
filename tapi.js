a6ce553dd23cd99f62206c890c1e2054e5b4facd
stiven miguel polo jimenez <147849147+stivenwoas@users.noreply.github.com>
2025-06-12 00:26:34 -0500

    Add files via upload

---
+++ tapi.js
@@ -0,0 +1,14 @@
+function showModal(img) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const captionText = document.getElementById("caption");
+    
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
